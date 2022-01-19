f23d012a31e708a95c8c89e30ea3d27e938eb403
Luk4x <luk4xm4ci3l@gmail.com>
2022-01-19 11:07:39 -0300

    v1.0.2

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 navbar = document.getElementById('navBar')
+menuBtn = document.getElementById('menu-button')
 
 function showMenu() {
     if (navbar.style.display == 'block') {
         navbar.style.display = 'none'
+        menuBtn.style.marginRight = '0'
     } else {
         navbar.style.display = 'block'
+        menuBtn.style.marginRight = '5px'
     }
 }
